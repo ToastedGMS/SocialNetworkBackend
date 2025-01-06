a7486eae62141334794614fb4f300876d85c6508
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-01-06 17:15:06 -0300

    tests for dbReadUser function

--- prisma/tests/users.test.js
+++ prisma/tests/users.test.js
@@ -1,10 +1,11 @@
 const prisma = require('../prismaClient/prismaClient.cjs');
 const bcrypt = require('bcryptjs');
-const { dbCreateUser } = require('../scripts/users.cjs');
+const { dbCreateUser, dbReadUser } = require('../scripts/users.cjs');
 
 jest.mock('../prismaClient/prismaClient.cjs', () => ({
 	user: {
 		create: jest.fn(),
+		findFirst: jest.fn(),
 	},
 }));
 
@@ -104,7 +105,71 @@ describe('dbCreateUser', () => {
 		prisma.user.create.mockRejectedValue(new Error('Unexpected error'));
 
 		await expect(dbCreateUser({ userInfo })).rejects.toThrow(
-			'An unexpected error occurred. Please try again later.'
+			'An unexpected error occurred. Details: Unexpected error'
 		);
+	});
+});
+
+describe('dbReadUser', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	test('ensure that provided query is not null or empty', async () => {
+		const invalidQueries = [
+			{ userInfo: { id: null, email: null, username: null } },
+			{ userInfo: { id: '', email: '', username: '' } },
+			{ userInfo: {} },
+		];
+
+		for (const query of invalidQueries) {
+			await expect(dbReadUser(query)).rejects.toThrow(
+				'Search query may not be null or empty.'
+			);
+		}
+	});
+
+	test('ensure that function returns the user object when valid queries are passed', async () => {
+		prisma.user.findFirst.mockResolvedValue({
+			id: 1,
+			username: 'testuser',
+			email: 'test@example.com',
+			bio: 'This is a test bio',
+			profilePic: 'profile.jpg',
+		});
+
+		const validQueries = [
+			{ userInfo: { id: 1, email: 'test@example.com', username: 'testuser' } },
+			{ userInfo: { id: 1, username: 'testuser' } },
+			{ userInfo: { email: 'test@example.com' } },
+		];
+
+		for (const query of validQueries) {
+			const result = await dbReadUser(query);
+
+			expect(result).toEqual({
+				id: 1,
+				username: 'testuser',
+				email: 'test@example.com',
+				bio: 'This is a test bio',
+				profilePic: 'profile.jpg',
+			});
+		}
+	});
+
+	test('should throw an error if user is not found', async () => {
+		prisma.user.findFirst.mockResolvedValue(null);
+
+		const queries = [
+			{ userInfo: { id: 2 } },
+			{ userInfo: { email: 'test2@coolmail.com' } },
+			{ userInfo: { username: 'testuser2' } },
+		];
+
+		for (const query of queries) {
+			await expect(dbReadUser(query)).rejects.toThrow(
+				'Provided user was not found. Please try again.'
+			);
+		}
 	});
 });
