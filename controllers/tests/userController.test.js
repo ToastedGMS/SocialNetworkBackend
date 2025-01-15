ccde3ba2b5f247d5e6e2569eae1beddff96ed2d4
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-01-15 15:47:51 -0300

    add user related controller functions and related tests
    I forgot to do individual commits sorry :(

---
+++ controllers/tests/userController.test.js
@@ -0,0 +1,343 @@
+const {
+	createUser,
+	readUser,
+	updateUser,
+	deleteUser,
+} = require('../userController.cjs');
+const {
+	dbCreateUser,
+	dbReadUser,
+	dbUpdateUser,
+	dbDeleteUser,
+} = require('../../prisma/scripts/users.cjs');
+
+const prisma = require('../../prisma/prismaClient/prismaClient.cjs');
+jest.mock('../../prisma/prismaClient/prismaClient.cjs', () => ({
+	user: {
+		findFirst: jest.fn(),
+	},
+}));
+
+jest.mock('../../prisma/scripts/users.cjs', () => {
+	return {
+		dbCreateUser: jest.fn(),
+		dbReadUser: jest.fn(),
+		dbUpdateUser: jest.fn(),
+		dbDeleteUser: jest.fn(),
+	};
+});
+
+describe('createUser controller', () => {
+	let req, res;
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+		req = { body: {} };
+		res = {
+			status: jest.fn().mockReturnThis(),
+			json: jest.fn(),
+		};
+	});
+
+	test('returns a 400 error when required fields are missing', async () => {
+		req.body = {
+			username: 'testuser',
+			// Missing email
+			password: '12345678',
+		};
+
+		const error =
+			'All required fields (username, email, password) must be provided.';
+		dbCreateUser.mockRejectedValue(new Error(error));
+
+		await createUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.json).toHaveBeenCalledWith({ error: error });
+	});
+
+	test('returns a 400 error when special characters are included', async () => {
+		req.body = {
+			username: 'test_user!',
+			email: 'test@example.com',
+			password: '12345678',
+		};
+
+		const error = 'Username may not contain any special characters';
+		dbCreateUser.mockRejectedValue(new Error(error));
+
+		await createUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.json).toHaveBeenCalledWith({ error: error });
+	});
+
+	test('returns a bad request (400) error in case of bad inputs', async () => {
+		req.body = {
+			username: 'testuser',
+			email: 'test@example.com',
+			password: 'nope',
+		};
+
+		const error = 'Password must be at least 8 characters long.';
+		dbCreateUser.mockRejectedValue(new Error(error));
+
+		await createUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.json).toHaveBeenCalledWith({ error: error });
+	});
+
+	test('returns a 500 error for unexpected server errors', async () => {
+		req.body = {
+			username: 'testuser',
+			email: 'test@example.com',
+			password: '12345678',
+		};
+
+		const error = 'Unexpected database error';
+		dbCreateUser.mockRejectedValue(new Error(error));
+
+		await createUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith({ error: 'Internal server error' });
+	});
+
+	test('returns a 201 and the user object as json when successful', async () => {
+		const user = {
+			id: 1,
+			username: 'testuser',
+			email: 'test@example.com',
+			profilePic: 'testprofile.png',
+			bio: 'test bio idk',
+		};
+
+		req.body = {
+			username: 'testuser',
+			email: 'test@example.com',
+			password: '12345678',
+		};
+
+		dbCreateUser.mockResolvedValue(user);
+
+		await createUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(201);
+		expect(res.json).toHaveBeenCalledWith(user);
+	});
+});
+
+describe('readUser controller', () => {
+	let req, res;
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+		req = { body: {} };
+		res = {
+			status: jest.fn().mockReturnThis(),
+			json: jest.fn(),
+		};
+	});
+
+	test('returns a 400 error for empty user search queries', async () => {
+		const error = 'Search query may not be null or empty.';
+
+		dbReadUser.mockRejectedValue(new Error(error));
+
+		await readUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.json).toHaveBeenCalledWith({ error: error });
+	});
+
+	test('returns a 404 error if user is not found', async () => {
+		const error = 'Provided user was not found. Please try again.';
+
+		dbReadUser.mockRejectedValue(new Error(error));
+
+		await readUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(404);
+		expect(res.json).toHaveBeenCalledWith({ error: error });
+	});
+	test('returns a 500 error for unexpected database errors', async () => {
+		const error = 'Unexpected database error';
+
+		dbReadUser.mockRejectedValue(new Error(error));
+
+		await readUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith({ error: 'Internal server error' });
+	});
+	test('returns a 200 code with the user object for successful requests', async () => {
+		const user = {
+			id: 1,
+			username: 'testuser',
+			email: 'test@example.com',
+			profilePic: 'testprofile.png',
+			bio: 'test bio idk',
+		};
+
+		req.body = {
+			username: 'testuser',
+			email: 'test@example.com',
+			id: 1,
+		};
+
+		dbReadUser.mockResolvedValue(user);
+
+		await readUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(200);
+		expect(res.json).toHaveBeenCalledWith(user);
+	});
+});
+
+describe('updateUser controller', () => {
+	let req, res;
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+		req = { body: {} };
+		res = {
+			status: jest.fn().mockReturnThis(),
+			json: jest.fn(),
+		};
+	});
+
+	test('returns a 400 error when special characters are included', async () => {
+		req.body = {
+			id: 1,
+			username: 'test_user!',
+			email: 'test@example.com',
+			bio: null,
+			profilePic: 'pic.png',
+		};
+
+		const error = 'Username may not contain any special characters';
+		dbUpdateUser.mockRejectedValue(new Error(error));
+
+		await updateUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(400);
+		expect(res.json).toHaveBeenCalledWith({ error: error });
+	});
+
+	test('returns a message indicating no updates when values provided are not different', async () => {
+		req.body = {
+			id: 1,
+			username: 'testuser',
+			email: 'test@example.com',
+			bio: null,
+			profilePic: 'pic.png',
+		};
+		prisma.user.findFirst.mockResolvedValue({
+			id: 1,
+			username: 'testuser',
+			email: 'test@example.com',
+			bio: null,
+			profilePic: 'pic.png',
+		});
+
+		const message = 'No values to update.';
+		dbUpdateUser.mockResolvedValue(message);
+
+		await updateUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(200);
+		expect(res.json).toHaveBeenCalledWith({ message: message });
+	});
+
+	test('returns a 500 error for unexpected database errors', async () => {
+		const error = 'Unexpected database error';
+		dbUpdateUser.mockRejectedValue(new Error(error));
+
+		await updateUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith({ error: 'Internal server error' });
+	});
+
+	test('returns a 200 with the user object on successful requests', async () => {
+		req.body = {
+			id: 1,
+			username: 'testuser',
+			email: 'test@example.com',
+			bio: null,
+			profilePic: 'pic.png',
+		};
+		dbUpdateUser.mockResolvedValue({
+			id: 1,
+			username: 'testuser',
+			email: 'test@example.com',
+			bio: null,
+			profilePic: 'pic.png',
+		});
+
+		await updateUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(200);
+		expect(res.json).toHaveBeenCalledWith({
+			id: 1,
+			username: 'testuser',
+			email: 'test@example.com',
+			bio: null,
+			profilePic: 'pic.png',
+		});
+	});
+});
+
+describe('deleteUser controller', () => {
+	let req, res;
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+		req = { body: {} };
+		res = {
+			status: jest.fn().mockReturnThis(),
+			json: jest.fn(),
+		};
+	});
+
+	test('returns a 403 error if password does not match', async () => {
+		req.body = {
+			id: 1,
+			password: 'incorrectPassword',
+		};
+
+		const error = 'Forbidden action: Password does not match.';
+		dbDeleteUser.mockRejectedValue(new Error(error));
+
+		await deleteUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(403);
+		expect(res.json).toHaveBeenCalledWith({ error: error });
+	});
+
+	test('returns a 500 error for unexpected database errors', async () => {
+		const error = 'Unexpected database error';
+		dbDeleteUser.mockRejectedValue(new Error(error));
+
+		await deleteUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith({ error: 'Internal server error' });
+	});
+
+	test('returns a 200 success message when user is deleted successfully', async () => {
+		req.body = {
+			id: 1,
+			password: 'correctPassword',
+		};
+
+		const message = 'User deleted successfully.';
+		dbDeleteUser.mockResolvedValue(message);
+
+		await deleteUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(200);
+		expect(res.json).toHaveBeenCalledWith({ message: message });
+	});
+});
---
+++ controllers/userController.cjs
@@ -0,0 +1,91 @@
+const {
+	dbCreateUser,
+	dbReadUser,
+	dbUpdateUser,
+	dbDeleteUser,
+} = require('../prisma/scripts/users.cjs');
+
+async function createUser(req, res) {
+	try {
+		const { username, email, password, bio, profilePic } = req.body;
+
+		const newUser = await dbCreateUser({
+			userInfo: { username, email, password, bio, profilePic },
+		});
+
+		return res.status(201).json(newUser);
+	} catch (error) {
+		console.error('Error creating user:', error);
+
+		if (
+			error.message.includes('required fields') ||
+			error.message.includes('special characters') ||
+			error.message.includes('at least 8 characters')
+		) {
+			return res.status(400).json({ error: error.message });
+		}
+
+		return res.status(500).json({ error: 'Internal server error' });
+	}
+}
+
+async function readUser(req, res) {
+	try {
+		const { id, email, username } = req.body;
+
+		const user = await dbReadUser({ userInfo: { id, username, email } });
+
+		return res.status(200).json(user);
+	} catch (error) {
+		console.error('Error reading user:', error);
+
+		if (error.message.includes('null or empty.')) {
+			return res.status(400).json({ error: error.message });
+		}
+		if (error.message.includes('was not found')) {
+			return res.status(404).json({ error: error.message });
+		}
+		return res.status(500).json({ error: 'Internal server error' });
+	}
+}
+
+async function updateUser(req, res) {
+	try {
+		const { id, username, email, bio, profilePic } = req.body;
+		const updatedUser = await dbUpdateUser({
+			userInfo: { id, username, email, bio, profilePic },
+		});
+		if (updatedUser === 'No values to update.') {
+			return res.status(200).json({ message: 'No values to update.' });
+		}
+
+		return res.status(200).json(updatedUser);
+	} catch (error) {
+		console.error('Error updating user:', error);
+
+		if (error.message.includes('special characters')) {
+			return res.status(400).json({ error: error.message });
+		}
+
+		return res.status(500).json({ error: 'Internal server error' });
+	}
+}
+
+async function deleteUser(req, res) {
+	try {
+		const { id, password } = req.body;
+		const deletionMessage = await dbDeleteUser({ userInfo: { id, password } });
+
+		return res.status(200).json({ message: deletionMessage });
+	} catch (error) {
+		console.error('Error deleting user:', error);
+
+		if (error.message.includes('Forbidden action: Password does not match')) {
+			return res.status(403).json({ error: error.message });
+		}
+
+		return res.status(500).json({ error: 'Internal server error' });
+	}
+}
+
+module.exports = { createUser, readUser, updateUser, deleteUser };
