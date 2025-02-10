21495bc57989a89b48d4ae426453131101728321
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-02-10 09:06:01 -0300

    change ID source from body to req.query

--- controllers/tests/friendshipController.test.js
+++ controllers/tests/friendshipController.test.js
@@ -78,7 +78,7 @@ describe('Friendship Controller', () => {
 
 	describe('getFriendshipStatus', () => {
 		it('should return friendship status and status 200', async () => {
-			const req = { body: { senderId: 1, receiverId: 2 } };
+			const req = { query: { senderId: 1, receiverId: 2 } };
 			const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 
 			dbGetFriendshipStatus.mockResolvedValue({
@@ -100,7 +100,7 @@ describe('Friendship Controller', () => {
 		});
 
 		it('should return status 400 if missing parameters', async () => {
-			const req = { body: {} };
+			const req = { query: {} };
 			const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 
 			await getFriendshipStatus(req, res);
@@ -112,7 +112,7 @@ describe('Friendship Controller', () => {
 		});
 
 		it('should return status 404 if friendship not found', async () => {
-			const req = { body: { senderId: 1, receiverId: 2 } };
+			const req = { query: { senderId: 1, receiverId: 2 } };
 			const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 
 			dbGetFriendshipStatus.mockRejectedValue(
@@ -126,7 +126,7 @@ describe('Friendship Controller', () => {
 		});
 
 		it('should return status 500 if an internal error occurs', async () => {
-			const req = { body: { senderId: 1, receiverId: 2 } };
+			const req = { query: { senderId: 1, receiverId: 2 } };
 			const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 
 			dbGetFriendshipStatus.mockRejectedValue(new Error('Some internal error'));
