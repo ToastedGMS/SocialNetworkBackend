2ecffbdb07dabe537493d7fa0360f4137b9c1ed2
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-03-01 18:24:15 -0300

    UPDATE SERVICE ACCOUNT FORMAT

--- firebase.cjs
+++ firebase.cjs
@@ -1,16 +1,10 @@
 const admin = require('firebase-admin');
-const serviceAccount = {
-	type: process.env.FIREBASE_TYPE,
-	project_id: process.env.FIREBASE_PROJECT_ID,
-	private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-	privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-	client_email: process.env.FIREBASE_CLIENT_EMAIL,
-	client_id: process.env.FIREBASE_CLIENT_ID,
-	auth_uri: process.env.FIREBASE_AUTH_URI,
-	token_uri: process.env.FIREBASE_TOKEN_URI,
-	auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-	client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-};
+require('dotenv').config(); // Load environment variables from .env file
+
+const encodedJson = process.env.ENCODED_SERVICE_ACCOUNT; // Get the base64 encoded string from .env
+const jsonString = Buffer.from(encodedJson, 'base64').toString('utf-8'); // Decode it
+
+const serviceAccount = JSON.parse(jsonString); // Parse it into a JSON object
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
