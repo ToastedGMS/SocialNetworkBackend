44866a5723c1fc3ccbbf6770e96b902387b75a62
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-03-01 13:46:11 -0300

    UPDATE FIREBASE ACCESS

--- firebase.cjs
+++ firebase.cjs
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('/home/gabrielmgs/Downloads/file-uploader42069-firebase-adminsdk-1th49-7328f85525.json');
+const serviceAccount = process.env.FIREBASE_ADMIN_SDK;
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
